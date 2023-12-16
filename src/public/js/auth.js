41db2accfd49befcc5d0e991e1dc5e96d788d30d
Jorge Renteria <jorge_9_90@hotmail.com>
2023-12-16 14:58:29 -0500

    update

--- src/public/js/auth.js
+++ src/public/js/auth.js
@@ -6,7 +6,7 @@ registerForm?.addEventListener("submit", async (event) => {
   const data = new FormData(registerForm);
   const registerPayload = Object.fromEntries(data);
 
-  await fetch("http://localhost:8080/api/session/register", {
+  await fetch("/api/session/register", {
     method: "POST",
     body: JSON.stringify(registerPayload),
     headers: {
@@ -28,7 +28,7 @@ loginForm?.addEventListener("submit", async (event) => {
   const data = new FormData(loginForm);
   const loginPayload = Object.fromEntries(data);
 
-  await fetch("http://localhost:8080/api/session/login", {
+  await fetch("/api/session/login", {
     method: "POST",
     body: JSON.stringify(loginPayload),
     headers: {
